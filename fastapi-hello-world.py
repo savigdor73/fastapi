3d3dd6d38da5a5be3be01e9ab594089b8a146f22
savigdor73 <avigdor.ads@gmail.com>
2024-08-31 12:30:51 +0300

    Update fastapi-hello-world.py

--- fastapi-hello-world.py
+++ fastapi-hello-world.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.options("/")
 def options_root():
     return JSONResponse(headers={"Allow": "GET, POST, OPTIONS"}, status_code=200)
-
+    
 @app.post("/pabbly")
 async def handle_pabbly(request: Request):
     body = await request.json()

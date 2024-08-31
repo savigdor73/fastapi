6fe86a002473d7c7d6bc31bb8181959f03197d15
savigdor73 <avigdor.ads@gmail.com>
2024-08-31 11:54:40 +0300

    Update fastapi-hello-world.py

--- fastapi-hello-world.py
+++ fastapi-hello-world.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/pabbly")
+def read_pabbly():
+    return {"message": "pabbly just sent you a message"}

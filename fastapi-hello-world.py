9b6d7dea0c3b5945897b256ebc8122bf8f157df9
savigdor73 <avigdor.ads@gmail.com>
2024-08-31 12:38:03 +0300

    Update fastapi-hello-world.py

--- fastapi-hello-world.py
+++ fastapi-hello-world.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # אפשר להתאים לפי הצורך
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.options("/")
 def options_root():

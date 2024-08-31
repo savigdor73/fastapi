4f4eb1de89fef730e231d1c5c281ff37866a729f
savigdor73 <avigdor.ads@gmail.com>
2024-08-31 12:13:28 +0300

    Update fastapi-hello-world.py

--- fastapi-hello-world.py
+++ fastapi-hello-world.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+@app.options("/")
+def options_root():
+    return JSONResponse(headers={"Allow": "GET, POST, OPTIONS"}, status_code=200)
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
